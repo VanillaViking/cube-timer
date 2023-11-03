8601e5ff247b13a3b9cd23f9cf2a68b720e38275
Ashwin Rajesh <46510831+VanillaViking@users.noreply.github.com>
2023-11-03 13:21:32 +1100

    feat: working timer component (#2)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center p-12 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center p-12 bg-black text-white ${inter.className}`}
     >
 
       <ScrambleHeader test="Hello World" />
