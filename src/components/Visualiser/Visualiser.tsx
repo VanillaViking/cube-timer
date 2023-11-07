ef94340afadec5b2b7edad9f3dbb0f884e45b9ac
Vanilla Viking <ashwinr2k2@gmail.com>
2023-11-07 16:23:45 +1100

    feat: i think it workds

--- src/components/Visualiser/Visualiser.tsx
+++ src/components/Visualiser/Visualiser.tsx
@@ -2,9 +2,10 @@ import {useRubiksCube} from "@/hooks/useRubiksCube"
 import Face from "./Face"
 
 const Visualiser = ({scramble}: {scramble: string}) => {
-    const {rubiksCube: cube} = useRubiksCube(); 
+    const {rubiksCube: cube, turn} = useRubiksCube(); 
 
     return (
+    <>
       <div className='grid grid-cols-4 gap-4'>
         <div className='w-48 h-48'/>
         <Face stickers={cube.UFace} />
@@ -19,6 +20,21 @@ const Visualiser = ({scramble}: {scramble: string}) => {
         <div className='w-48 h-48'/>
         <div className='w-48 h-48'/>
       </div>
+      <div className="grid grid-cols-6 gap-2 mt-8">
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("F")}>F</button>
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("R")}>R</button>
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("U")}>U</button>
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("L")}>L</button>
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("D")}>D</button>
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("B")}>B</button>
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("F'")}>F'</button>
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("R'")}>R'</button>
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("U'")}>U'</button>
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("L'")}>L'</button>
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("D'")}>D'</button>
+        <button className='border w-[100px] h-[40px] border-slate-500 rounded-lg bg-slate-300' onClick={() => turn("B'")}>B'</button>
+      </div>
+    </>
     )
 }
 
--- src/hooks/useRubiksCube.ts
+++ src/hooks/useRubiksCube.ts
@@ -18,6 +18,7 @@ export type RubiksCube = {
     DFace: Colours[],
 }
 
+
 const solvedCube = {
      UFace: Array(9).fill(Colours.WHITE),
      LFace: Array(9).fill(Colours.ORANGE),
@@ -27,23 +28,127 @@ const solvedCube = {
      DFace: Array(9).fill(Colours.YELLOW),
 }
 
+const layerConnections = {
+    UFace: {
+        top: {face: "BFace", connectedStickers: [2, 1, 0]},
+        left: {face: "LFace", connectedStickers: [2, 1, 0]},
+        bottom: {face: "FFace", connectedStickers: [2, 1, 0]},
+        right: {face: "RFace", connectedStickers: [2, 1, 0]},
+    },
+    LFace: {
+        top: {face: "UFace", connectedStickers: [0, 3, 6]},
+        left: {face: "BFace", connectedStickers: [8, 5, 2]},
+        bottom: {face: "DFace", connectedStickers: [0, 3, 6]},
+        right: {face: "FFace", connectedStickers: [0, 3, 6]},
+    },
+    FFace: {
+        top: {face: "UFace", connectedStickers: [6, 7, 8]},
+        left: {face: "LFace", connectedStickers: [8, 5, 2]},
+        bottom: {face: "DFace", connectedStickers: [2, 1, 0]},
+        right: {face: "RFace", connectedStickers: [0, 3, 6]},
+    },
+    RFace: {
+        top: {face: "UFace", connectedStickers: [2, 5, 8]},
+        left: {face: "FFace", connectedStickers: [2, 5, 8]},
+        bottom: {face: "DFace", connectedStickers: [2, 5, 8]},
+        right: {face: "BFace", connectedStickers: [6, 3, 0]},
+    },
+    BFace: {
+        top: {face: "UFace", connectedStickers: [2, 1, 0]},
+        left: {face: "RFace", connectedStickers: [8, 5, 2]},
+        bottom: {face: "DFace", connectedStickers: [6, 7, 8]},
+        right: {face: "BFace", connectedStickers: [6, 3, 0]},
+    },
+    DFace: {
+        top: {face: "FFace", connectedStickers: [6, 7, 8]},
+        left: {face: "LFace", connectedStickers: [6, 7, 8]},
+        bottom: {face: "BFace", connectedStickers: [6, 7, 8]},
+        right: {face: "RFace", connectedStickers: [6, 7, 8]},
+    },
+}
+
 export function useRubiksCube(initialCube?: RubiksCube) {
     const [rubiksCube, setRubiksCube] = useState<RubiksCube>(initialCube || solvedCube);
 
     const turn = (move: string) => {
         const face = `${move[0]}Face`
-        const anticlockwise = !!move[1]
+        const anticlockwise = move.includes("'")
+        const double = move.includes("2")
 
-        console.log(anticlockwise)
+        const rotatedFace = rotateFace(face, anticlockwise, double)
+        rotateAdjacent(face, anticlockwise, double, rotatedFace)
+
     }
 
-    const rotateFace = () => {
+    const rotateFace = (face: string, anticlockwise: boolean, double: boolean) => {
+        // cbbs
+        //@ts-ignore
+        const currentFace = structuredClone(rubiksCube[face])
+        let newFace: Colours[];
+        if (anticlockwise)  {
+            newFace = [currentFace[2], currentFace[5], currentFace[8], currentFace[1], currentFace[4], currentFace[7], currentFace[0], currentFace[3], currentFace[6]]
+        } else if (double) {
+            newFace = currentFace.reverse()
+        }
+        else {
+            newFace = [currentFace[6], currentFace[3], currentFace[0], currentFace[7], currentFace[4], currentFace[1], currentFace[8], currentFace[5], currentFace[2]]
+        }
+
+        return newFace
         
+    }
+
+    const rotateAdjacent = (face: string, anticlockwise: boolean, double: boolean, rotatedFace: Colours[]) => {
+        //@ts-ignore
+        const adjacentConnections = layerConnections[face]
+        let editedFaces = structuredClone(rubiksCube)
+        //@ts-ignore
+        editedFaces[face] = rotatedFace
+
+
+        if (anticlockwise) {
+            for (let i = 0; i < 3; i++) {
+                // quality code
+                //@ts-ignore
+                editedFaces[adjacentConnections.left.face][adjacentConnections.left.connectedStickers[i]] = rubiksCube[adjacentConnections.top.face][adjacentConnections.top.connectedStickers[i]]
+                //@ts-ignore
+                editedFaces[adjacentConnections.bottom.face][adjacentConnections.bottom.connectedStickers[i]] = rubiksCube[adjacentConnections.left.face][adjacentConnections.left.connectedStickers[i]]
+                //@ts-ignore
+                editedFaces[adjacentConnections.right.face][adjacentConnections.right.connectedStickers[i]] = rubiksCube[adjacentConnections.bottom.face][adjacentConnections.bottom.connectedStickers[i]]
+                //@ts-ignore
+                editedFaces[adjacentConnections.top.face][adjacentConnections.top.connectedStickers[i]] = rubiksCube[adjacentConnections.right.face][adjacentConnections.right.connectedStickers[i]]
+            }
+        } else if (double) {
+            for (let i = 0; i < 3; i++) {
+                //@ts-ignore
+                editedFaces[adjacentConnections.right.face][adjacentConnections.right.connectedStickers[i]] = rubiksCube[adjacentConnections.left.face][adjacentConnections.left.connectedStickers[i]]
+                //@ts-ignore
+                editedFaces[adjacentConnections.bottom.face][adjacentConnections.bottom.connectedStickers[i]] = rubiksCube[adjacentConnections.top.face][adjacentConnections.top.connectedStickers[i]]
+                //@ts-ignore
+                editedFaces[adjacentConnections.left.face][adjacentConnections.left.connectedStickers[i]] = rubiksCube[adjacentConnections.right.face][adjacentConnections.right.connectedStickers[i]]
+                //@ts-ignore
+                editedFaces[adjacentConnections.top.face][adjacentConnections.top.connectedStickers[i]] = rubiksCube[adjacentConnections.bottom.face][adjacentConnections.bottom.connectedStickers[i]]
+            }
+        } else {
+
+            for (let i = 0; i < 3; i++) {
+                //@ts-ignore
+                editedFaces[adjacentConnections.right.face][adjacentConnections.right.connectedStickers[i]] = rubiksCube[adjacentConnections.top.face][adjacentConnections.top.connectedStickers[i]]
+                //@ts-ignore
+                editedFaces[adjacentConnections.bottom.face][adjacentConnections.bottom.connectedStickers[i]] = rubiksCube[adjacentConnections.right.face][adjacentConnections.right.connectedStickers[i]]
+                //@ts-ignore
+                editedFaces[adjacentConnections.left.face][adjacentConnections.left.connectedStickers[i]] = rubiksCube[adjacentConnections.bottom.face][adjacentConnections.bottom.connectedStickers[i]]
+                //@ts-ignore
+                editedFaces[adjacentConnections.top.face][adjacentConnections.top.connectedStickers[i]] = rubiksCube[adjacentConnections.left.face][adjacentConnections.left.connectedStickers[i]]
+            }
+
+        }
+
+        setRubiksCube(editedFaces)
     }
 
     return {
         rubiksCube,
-        setRubiksCube,
         turn,
     }
 }
